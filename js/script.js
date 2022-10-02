4f9da7a8e71330f6b4abfc1730d3956e228a72f7
Bjorn Jakobsen <106143706+Ukjeent@users.noreply.github.com>
2022-10-02 22:51:16 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,130 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
+
+  ///////////////
+ // SearchBar //
+///////////////
+
+// Append a searchbar
+const header = document.querySelector('.header');
+const SearchBarHtml = `
+   <label for="search" class="student-search">
+   <span>Search by name</span>
+   <input id="search" onkeyup="searchfunction(data)" placeholder="Search by name...">
+   <button id ="searchbtn" type="button"><img id="searchBtnImg" src="img/icn-search.svg" alt="Search icon"></button>
+   </label>`;
+header.insertAdjacentHTML("beforeend", SearchBarHtml);
+
+  //////////////////////////
+ // Search functionality //
+//////////////////////////
+
+// Checks if a student name or email matches searchinput before adding the student to the studentList.
+
+const studentList = document.querySelector('.student-list');
+
+function searchfunction(list){
+   const searchInput = document.getElementById('search');
+   const inputValue = searchInput.value.toLowerCase();
+
+
+   studentList.innerHTML = '';
+
+   for (let i = 0; i < list.length; i++) {
+      if (list[i].name.first.toLowerCase().includes(inputValue) || list[i].name.last.toLowerCase().includes(inputValue) || list[i].email.toLowerCase().includes(inputValue)) {
+         const html = `
+         <li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+           <h3>${list[i].name.first} ${list[i].name.last}</h3>
+           <span class="email">${list[i].email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">Joined ${list[i].registered.date}</span>
+         </div>
+       </li>
+      `;
+      studentList.insertAdjacentHTML("beforeend", html);
+      }
+   }
+}
+
+ // calls searchfunction when a user clicks on the searchbutton
+document.addEventListener('click', (e) => {
+   const clickTarget = e.target;
+   if (e.target.id === 'searchbtn' || e.target.id === 'searchBtnImg') {
+      searchfunction(data)
+   }
+});
+
+  //////////////////
+ // StudentList //
+////////////////
 
 
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+// Creates a list of students and appends the list to the page. 
+function showPage(list, page) {
+   studentList.innerHTML = '';
+   const startIndex = (page * 9) - 9;
+   endIndex = page * 9;
+
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         const html = `
+         <li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+           <h3>${list[i].name.first} ${list[i].name.last}</h3>
+           <span class="email">${list[i].email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">Joined ${list[i].registered.date}</span>
+         </div>
+       </li>
+      `;
+      studentList.insertAdjacentHTML("beforeend", html);
+   }
+   }
+}
 
 
+  /////////////////
+ // Pageination //
+/////////////////
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+const linkList = document.querySelector('.link-list');
+linkList.innerHTML = '';
 
+// Creates list elements / pagebuttons and appends the buttons to the site. 
+function addPagination(list) {
+   const numOfPages = Math.ceil(list.length / 9);
+   for (let i = 0; i < numOfPages; i++) {
+      linkList.innerHTML += `
+      <li>
+      <button type="button">${i+1}</button>
+    </li>`;
+   }
+   const firstButton = document.querySelector('button');
+   firstButton.classList.add('active')
+}
 
+// Listens for button clicks, removes active from all buttons and adds the active class to the clicktarget.
+// Calls the showPage function to update the page. 
+linkList.addEventListener('click', (e) => {
+   const clickTarget = e.target;
+   const pageBtn = document.querySelectorAll('.link-list button');
+   let pageNum = 1;
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+   if (clickTarget.type === "button") {
+      pageBtn.forEach(element => element.classList.remove('active'));
+      clickTarget.classList.add('active');
+      pageNum = clickTarget.innerHTML;
+   }
+   showPage(data, pageNum)
+});
 
+  ////////////////////
+ // Call functions //
+////////////////////
 
-
-// Call functions
+showPage(data, 1);
+addPagination(data) 
